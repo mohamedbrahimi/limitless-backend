59b88414b20aa764f696c27bbf725172afcc2b5b
mohamedbrahimi <mohkriss3@gmail.com>
2019-10-01 15:40:46 +0100

    added check MW index

--- src/middlewars/user/checkUniqueIndexes.js
+++ src/middlewars/user/checkUniqueIndexes.js
@@ -1,13 +1,39 @@
-//import
-module.exports = (req, res, next) => {
-    try {
+import User from '../../models/user';
 
-        const userBody = req.body;
-        // const exist =
-        next();
-    } catch (error) {
-        return res.status(501).json({
-            message: 'failed to add new user'
-        });
+module.exports = async (req, res, next) => {
+
+    const { username, email, phone } = req.body;
+
+    /**
+     * check email, username, phone if are unique
+     */
+
+    const existedUsername = await User.findOne({ username });
+    if (existedUsername)
+        return res.status(443).json({
+            error: false,
+            errormessage: "username is already exist",
+            errormessageKey: "USERNAME_EXISTED"
+        })
+
+    const existedEmail = await User.findOne({ email });
+    if (existedEmail)
+        return res.status(443).json({
+            error: false,
+            errormessage: "email is already exist",
+            errormessageKey: "EMAIL_EXISTED"
+        })
+
+    if (phone) {
+        const existedPhone = await User.findOne({ phone });
+        if (existedPhone)
+            return res.status(443).json({
+                error: false,
+                errormessage: "phone is already exist",
+                errormessageKey: "PHONE_EXISTED"
+            })
     }
+
+    next();
+
 };
