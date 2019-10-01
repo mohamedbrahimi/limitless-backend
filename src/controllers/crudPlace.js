3081cb980f23900ebe288b4502ca2bf9b7594a3e
mohbadjah <mohamed.badjah@yatechnologies.com>
2019-10-01 15:34:40 +0100

    update savePlace() controller

--- src/controllers/crudPlace.js
+++ src/controllers/crudPlace.js
@@ -2,21 +2,27 @@ import Place from '../models/place';
 
 async function savePlace(req, res) {
 
-  const { name, title, description, coordinates } = req.body;
+  const { name, title, description, location } = req.body;
 
-  if (!coordinates.lat || !coordinates.lng) {
+  if (!location || !location.lat || !location.lng) {
     return res.status(400).json({
       success: false,
       error: 'Place coordinates is not valid',
       errorKey: 'PLACE_COORDINATES_NOT_VALID',
     });
   }
+
   try {
-    const place = new Place.create({
-      name,
-      title,
-      description,
-      coordinates,
+    const locationObj = {
+      lat: Number.parseFloat(location.lat),
+      lng: Number.parseFloat(location.lng)
+    }
+
+    const place = await Place.create({
+      ...(name) && { name },
+      ...(title) && { title },
+      ...(description) && { description },
+      location: locationObj
     })
 
     return res.status(200).json(place);
