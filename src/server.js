8fed785151e7e7a5f31f701a2a503f974bbf9858
mohamedbrahimi <mohkriss3@gmail.com>
2019-10-01 12:05:57 +0100

    review code

--- src/server.js
+++ src/server.js
@@ -36,6 +36,14 @@ mongoose.connect(config.mongodb.uri,
 );
 const connection = mongoose.connection;
 
+// CORS middleware
+const allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
 app.use(allowCrossDomain);
 app.use('/api/v1/public/b2c/client', publicClientRouter);
 
